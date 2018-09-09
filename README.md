f7102808e6165ac024300e1064d5d0d74769b6e1
Fernando Felix <fernandojr.ifcg@live.com>
2018-09-09 02:28:50 -0300

    Create initial project structure

---
+++ README.md
@@ -0,0 +1,42 @@
+# Scrapy Trial
+
+## Requirements
+
+- Python 3.5+
+- Virtualenv
+
+## Installation
+
+Run the following commands in the terminal to create a virtual environment and install requirements:
+
+```sh
+# create virtualenv
+sudo apt-get -y install python3-pip
+sudo pip3 install virtualenv
+virtualenv -p python3 .env && . .env/bin/activate
+
+# install python requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+## Usage
+
+To run the spider:
+```sh
+TODO
+```
+
+To test the spider:
+```sh
+TODO
+```
+
+To deploy the spider in Scrapy Cloud:
+```sh
+TODO
+```
+
+## Author
+
+- Fernando Felix do Nascimento Junior
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import sys
+import os
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements as parse
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements as parse
+
+
+from setuptools import find_packages
+
+requirements = lambda f: [str(i.req) for i in parse(f, session=False)]
+
+setuptools.setup(
+    name='scrapy-trial',
+    version='0.0.1',
+    author='Fernando Felix do Nascimento Junior',
+    author_email='fernandofelix@copin.ufcg.edu.br',
+    description='A Web Spider',
+    url='https://bitbucket.org/scrapinghub/fernando-do-nascimento-junior-scrapy-trial',
+    platforms='any',
+    license='None',
+    packages=find_packages(exclude=['tests']),
+    install_requires=requirements('requirements.txt'),
+    include_package_data=True,
+    zip_safe=False
+)
